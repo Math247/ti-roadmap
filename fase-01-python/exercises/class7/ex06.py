c8cb59b2d6f95862b83c44cc0251284f9183e83e
Matheus Carregal <matheuschabib@gmail.com>
2026-06-12 18:57:15 -0300

    feat: update class 7 exercises

--- fase-01-python/exercises/class7/ex06.py
+++ fase-01-python/exercises/class7/ex06.py
@@ -1,2 +1,2 @@
 n = int(input('Digite um número:'))
-print('O dobro de {} é {}. O triplo de {} é {}. A raiz quadrada de {} é {}.'.format(n, n*2, n, n*3, n, int(n**0.5)))
+print('O dobro de {} é {}. O triplo de {} é {}. A raiz quadrada de {} é {}.'.format(n, n*2, n, n*3, n, n**0.5))
